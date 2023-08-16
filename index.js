6fc8d2a750664a7455d7c7c342ee297ceb02256b
Bashir Ahmad <ahmaduafpk@yahoo.com>
2023-08-16 17:02:51 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,9 @@
+"use strict";
+//function abc (a:number,b:number):number
+//{
+//return a+b;
+//}
+//let c:number =abc(10,20);
+//console.log(c);
+let abc = (a, b) => a + b;
+console.log(abc(5, 6));
---
+++ index.ts
@@ -0,0 +1,10 @@
+//function abc (a:number,b:number):number
+//{
+//return a+b;
+//}
+//let c:number =abc(10,20);
+//console.log(c);
+
+let  abc = (a:number,b:number):number => a+b;
+
+console.log(abc(5,6));
